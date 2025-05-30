6e9e7c6bf3519ee0a6fdd199a8e2157ea428ab4d
Mixail <ssmitss58@gmail.com>
2025-05-30 13:04:30 +0300

    comleted second video confug ready

--- project/webpack.config.ts
+++ project/webpack.config.ts
@@ -1,40 +1,22 @@
-import path from "path";
-
 import webpack from "webpack";
 
-import HtmlWebpackPlugin from "html-webpack-plugin";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { Buildpaths } from "./build/types/config";
+import path from "path";
 
-const config:webpack.Configuration = {
-
-    
-  mode: "development",
-  entry: path.resolve(__dirname, "src", "index.ts"), //показывает где мы находимся,команда  __dirname тякущая папка
-  output: {
-    filename: "[name].[contenthash].js",
-
-    path: path.resolve(__dirname, "build"),
-    clean: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    new webpack.ProgressPlugin(),
-  ],
-  // module.rules
-  // Этот блок отвечает за обработку файлов, которые не являются обычными JavaScript-файлами (например, .ts, .tsx, .css, .scss, изображения и т. д.).
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/, //обрабатывает как ts так tsx
-        use: "ts-loader", //здесь мы используем ts-loader ,который позвоялет работать с ts файлами
-        exclude: /node_modules/, //искючаем node_modules
-      }, //rules обрабатывают лоудеры, они предназначены для обработки файлов, которые выходят за рамки js (pg,jpg,svg,css)
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"], //чтобы не писать расширение для этих файлов типо index.js
-  },
+const paths: Buildpaths = {
+  enrty: path.resolve(__dirname, "src", "index.ts"),
+  build: path.resolve(__dirname, "build"),
+  html: path.resolve(__dirname, "public", "index.html"),
 };
 
-export default config
+const mode = "development";
+const isDev = mode === "development";
+
+export const config: webpack.Configuration = buildWebpackConfig({
+  mode: "development",
+  paths,
+  isDev,
+});
+
+export default config;
