1e4a32a574d3e4e792e5488ecd14b19b1ee5214c
Mixail <ssmitss58@gmail.com>
2025-05-30 15:25:57 +0300

    fix second video

--- project/webpack.config.ts
+++ project/webpack.config.ts
@@ -1,11 +1,11 @@
 import webpack from "webpack";
 
 import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
-import { Buildpaths } from "./build/types/config";
+import { Buildpaths } from "build/types/config";
 import path from "path";
 
 const paths: Buildpaths = {
-  enrty: path.resolve(__dirname, "src", "index.ts"),
+  entry: path.resolve(__dirname, "src", "index.ts"),
   build: path.resolve(__dirname, "build"),
   html: path.resolve(__dirname, "public", "index.html"),
 };
