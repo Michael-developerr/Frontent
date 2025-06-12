6d5adf1ede44cbde2536dc960369aec87ba1b88e
Mixail <ssmitss58@gmail.com>
2025-06-12 20:06:44 +0300

    feat: просмотрел four  Подключаем React и настраиваем css в webpack меткаГиперссылка

---
+++ project/src/index.tsx
@@ -0,0 +1,8 @@
+import { createRoot } from "react-dom/client";
+import { Counter } from "./components/Counter";
+
+createRoot(document.getElementById("root")!).render(
+  <div>
+    <Counter />{" "}
+  </div>
+);
