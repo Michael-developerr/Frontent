689263c863be0422ceb45ef8685670cc4322886e
Mixail <ssmitss58@gmail.com>
2025-06-15 15:20:49 +0300

    feat: add Navbar

--- project/src/app/App.tsx
+++ project/src/app/App.tsx
@@ -6,15 +6,16 @@ import "./styles/index.scss";
 
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import AboutPage from "pages/AboutPage/ui/AboutPage";
-import MainPage from "pages/MainPage/ui/MainPage";
 import { AppRouter } from "./providers/route";
+import { NavBar } from "widgets/Navbar";
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
+
+      <NavBar/>
       <button onClick={toggleTheme}> TOGGLE</button>
       <Link to={"/"}>Главная страница</Link>
       <Link to={"/about"}>о сайте</Link>
