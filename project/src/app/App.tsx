fe98291548608adf83a33c49368b7060f7f653c8
Mixail <ssmitss58@gmail.com>
2025-06-16 10:12:48 +0300

    feat:start svg

--- project/src/app/App.tsx
+++ project/src/app/App.tsx
@@ -10,16 +10,12 @@ import { AppRouter } from "./providers/route";
 import { NavBar } from "widgets/Navbar";
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme,  } = useTheme();
 
   return (
     <div className={classNames("app", {}, [theme])}>
 
       <NavBar/>
-      <button onClick={toggleTheme}> TOGGLE</button>
-      <Link to={"/"}>Главная страница</Link>
-      <Link to={"/about"}>о сайте</Link>
-
       <AppRouter />
     </div>
   );
