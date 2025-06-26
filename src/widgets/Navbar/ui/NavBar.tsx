bb8dd7ea0ba0cfaca0c5d799dcb66e29c0ba27da
Mixail <ssmitss58@gmail.com>
2025-06-26 11:21:19 +0300

    fix:  delete AppLink and  AppLinkTheme   in Navbar

--- src/widgets/Navbar/ui/NavBar.tsx
+++ src/widgets/Navbar/ui/NavBar.tsx
@@ -1,21 +1,16 @@
 import { classNames } from "shared/lib/classNames/classNames";
 
 import cls from "./NavBar.module.scss";
-import AppLink, { AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
 interface NavBarProps {
   className?: string;
 }
 
- export const NavBar = ({ className }: NavBarProps) => {
+export const NavBar = ({ className }: NavBarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [className ?? ""])}>
       {/* <ThemeSwitcher/> */}
-      <div className={cls.links}>
-        /
-      </div>
+      <div className={cls.links}>/</div>
     </div>
   );
 };
-
-
