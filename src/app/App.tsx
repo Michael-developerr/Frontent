1322fc2199a4c4f8a0ca9aa995fdd40a8a2a8d3e
Mixail <ssmitss58@gmail.com>
2025-06-27 11:58:44 +0300

    feat: add Modal.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,22 +6,30 @@ import { AppRouter } from "./providers/route";
 import { NavBar } from "widgets/Navbar";
 import { SideBar } from "widgets/sideBar";
 
-import { Suspense,  } from "react";
+import { Suspense, useState } from "react";
+import { Modal } from "shared/ui/Modal/Modal";
 
 const App = () => {
   const { theme } = useTheme();
-
- 
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className={classNames("app", {}, [theme])}>
       <Suspense fallback={<div>Loading translations...</div>}>
         <NavBar />
+        <button onClick={() => setIsOpen(true)}>toggle</button>
+
+        <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+          lorem-ipsum is a JavaScript module for generating passages of lorem
+          ipsum text. Lorem ipsum text is commonly used as placeholder text in
+          publishing, graphic design, and web development.
+        </Modal>
+
+        <div className="content-page">
+          <SideBar />
+          <AppRouter />
+        </div>
       </Suspense>
-      <div className="content-page">
-        <SideBar />
-        <AppRouter />
-      </div>
     </div>
   );
 };
