fc688bed2270061bdbf8c54101246fc9916286ec
Mixail <ssmitss58@gmail.com>
2025-07-18 07:30:02 +0300

    feat: add tests  for login module

--- src/features/AuthByUserName/model/slice/loginSlice.test.ts
+++ src/features/AuthByUserName/model/slice/loginSlice.test.ts
@@ -1,8 +1,6 @@
 import { DeepPartial } from "utility-types";
 import { LoginSchema } from "../types/loginSchema";
 import { loginAction, loginReduser } from "./loginSlice";
-import { StateSchema } from "app/providers/StoreProvider";
-import { loginByUsername } from "../services/loginByUsername/loginByUsername";
 
 describe("loginSlice.test", () => {
   test("test set username", () => {
